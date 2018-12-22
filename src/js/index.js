1d6bf6d4d4167bb7113816bada3afea20caf0310
MirEnovi <garciaivonne11@gmail.com>
2018-12-22 16:16:09 -0600

    add slidenav

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+// alert('hola');
+document.addEventListener('DOMContentLoaded', () => {
+  // let elems = document.querySelectorAll('.sidenav');
+  // var instances = M.Sidenav.init(elems, options);
+  M.AutoInit();
+});
+
+// instance.open();
+// instance.close();
